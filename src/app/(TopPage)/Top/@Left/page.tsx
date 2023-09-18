71735aeabc923fefef4f504882b06f53632c4258
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-09-18 19:27:30 +0900

    なんで動いてないんですか？

--- src/app/(TopPage)/Top/@Left/page.tsx
+++ src/app/(TopPage)/Top/@Left/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { getProfile } from "@/libs/microcms"
+import ViTA from "@/Component/util/ViTA"
 import { Suspense,lazy } from "react"
 
 export default async function Left(){
     const {contents} = await getProfile()
-    const ViTA = lazy(() => import("@/Component/util/ViTA.jsx"))
     return (
         <>
             <Suspense>
