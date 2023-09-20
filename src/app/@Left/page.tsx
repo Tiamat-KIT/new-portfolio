d93f6e96b0f75a1802d77e1ae343bedb4095fcce
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-09-20 18:52:26 +0000

    これで動かす

--- src/app/@Left/page.tsx
+++ src/app/@Left/page.tsx
@@ -4,12 +4,12 @@ import ViTA from "@/Component/util/ViTA"
 export default async function Left(){
     const {contents} = await getProfile()
     return (
-        <>
+        <div className="px-4">
             <ViTA
                 ButtonText="泡沫君はどんな人？"
                 Default={<p>詳しく知る</p>}
                 After={contents[0]}
             />
-        </>
+        </div>
     )
 }
