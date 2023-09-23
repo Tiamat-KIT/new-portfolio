e078927d19d4ab5db9c8dcf82f19a70a11ce8531
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-09-23 06:35:18 +0000

    BlogCardのエラー修正

--- src/Component/util/BlogCard.tsx
+++ src/Component/util/BlogCard.tsx
@@ -1,7 +1,8 @@
+import { Route } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function BlogCard({title,link,img}:{title:string,link:string,img: string}){
+export default function BlogCard({title,link,img}:{title:string,link:Route,img: string}){
     return (
         <div className="card shadow-xl button flex-1">
             <figure><Image alt={title} src={img} /></figure>
